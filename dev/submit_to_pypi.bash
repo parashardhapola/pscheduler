c79135ba242d287f319905bf7b5a7c12ca70b6ad
parashardhapola <parashar.dhapola@gmail.com>
2016-11-11 13:07:06 +0530

    fixed setup.py typo

---
+++ dev/submit_to_pypi.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python setup.py register -r pypitest
+python setup.py sdist upload -r pypitest
+python setup.py register -r pypi
+python setup.py sdist upload -r pypi
+
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   description = 'A complete multi-remote machine job schedular package in Python',
   author = 'Parashar Dhapola',
   author_email = 'parashar.dhapola@gmail.com',
-  url = 'https://github.com/parashardhapola/pscheduler'
-  download_url = 'https://github.com/parashardhapola/pscheduler/tarball/0.0.1'
+  url = 'https://github.com/parashardhapola/pscheduler',
+  download_url = 'https://github.com/parashardhapola/pscheduler/tarball/0.0.1',
   keywords = ['scheduler', 'python', 'ssh']
 )
