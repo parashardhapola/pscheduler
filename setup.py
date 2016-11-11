16566dd088e949e4f0979d02702d471af9287d2b
parashardhapola <parashar.dhapola@gmail.com>
2016-11-11 10:52:53 +0530

    pypi ready

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'pscheduler',
+  packages = ['pscheduler'],
+  version = '0.0.1',
+  description = 'A complete multi-remote machine job schedular package in Python',
+  author = 'Parashar Dhapola',
+  author_email = 'parashar.dhapola@gmail.com',
+  url = 'https://github.com/parashardhapola/pscheduler'
+  download_url = 'https://github.com/parashardhapola/pscheduler/tarball/0.0.1'
+  keywords = ['scheduler', 'python', 'ssh']
+)
