6cb8d61b759eb478c749bbffb3582bb4c8c98a88
parashardhapola <parashar.dhapola@gmail.com>
2016-11-14 23:14:06 +0530

    release 0.0.4

--- dev/deploy.py
+++ dev/deploy.py
@@ -3,8 +3,9 @@ import os
 # run from one directory up
 
 version = open('VERSION').read()
-os.system('git tag %s -m "Add tag"' % version)
+os.system('git tag %s' % version)
 os.system('git push --tags origin master')
+
 os.system('python setup.py sdist bdist_wheel')
 os.system('twine register dist/pscheduler-*.whl')
 os.system('twine upload dist/*')
